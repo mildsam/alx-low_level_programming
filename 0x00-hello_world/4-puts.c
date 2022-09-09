6319b0932138b9d389d5329a85d74e7aa9824b58
mildsam <idakwosamuel2@gmail.com>
2022-09-08 20:13:45 -0700

    created my first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * declaring the entry point.. int .main()
+ * write the code block
+ * end the program ... return 0
+ */
+int main(void) /* main - Entry point*/
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0); /* ends the program*/
+}
