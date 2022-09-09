b43db4682362da5aae4d60b20f003a963ab1ab01
mildsam <idakwosamuel2@gmail.com>
2022-09-08 21:11:47 -0700

    run my second C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printf - prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: 1 if the number is true. 0 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
