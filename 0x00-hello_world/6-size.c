dff1c51459c11af6c60dbfba410be3fdd8ccd3bc
mildsam <idakwosamuel2@gmail.com>
2022-09-12 12:09:14 -0700

    sizes of various types in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ * printf: prints the size of the variables
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long longInt;
+	double doubleType;
+	float floatType;
+
+	/* sizeof evaluetes the size of a variable */
+	printf("size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("size of a long int: %zu byte(s)\n", sizeof(longInt));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+
+
+	return (0);
+}
