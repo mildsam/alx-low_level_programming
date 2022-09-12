94545b8e63b85da359b3a0f1f423e7d620d181ba
mildsam <idakwosamuel2@gmail.com>
2022-09-12 16:52:24 -0700

    print size of cdata types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,17 @@
 
 /**
  * main - entry point
- * printf: prints the size of the types
- * Return: Always 0 (Success)
+ *
+ * Return: 0
  */
+
 int main(void)
 {
-
-	/* sizeof evaluetes the size of a variable */
-	printf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n", sizeof(long));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(double));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
-
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
+
 }
