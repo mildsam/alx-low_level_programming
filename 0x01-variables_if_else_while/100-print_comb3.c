fac5d2defbf356c2c469bf63439cb8c946d7e115
mildsam <idakwosamuel2@gmail.com>
2022-09-12 17:01:07 -0700

    combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 48;
+	b = 48;
+
+	while (e < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			if (b != i && b < a)
+			{
+				putchar(b);
+				putchar(a);
+				if (a == 57 && b == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
