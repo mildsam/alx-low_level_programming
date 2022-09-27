05af74b5be546087ca8ae169b109bf3d32228e15
mildsam <idakwosamuel2@gmail.com>
2022-09-26 17:36:43 -0700

    Task 0 and 1 done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		s[i] = b;
 	}
 	_putchar('\n');
 	return (s);
